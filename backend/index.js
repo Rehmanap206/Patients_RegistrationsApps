dd85f9f6312769764ffce07c74ff82feb15f327b
moheebakhter <131138672+moheebakhter@users.noreply.github.com>
2025-04-27 23:42:57 +0500

    commit

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+let exp = require("express");
+require("dotenv").config();
+let r = require("./Routings/route");
+let db = require("./Db")
+let c = require("cors")
+let collection = require("./Collections/patients")
+let myapp = exp();
+let port = process.env.PORT || 7062;
+
+myapp.use(c());
+myapp.use(exp.json());
+myapp.use("/api/patients/",r);
+
+let sending_data = async function () {
+    try {
+        collection.create({
+            Full_Name: "Mudassir",
+            Age: 21,
+            Gender: "Male",
+            Phone_Number: "035689899",
+            Address: "ioobnfdn"
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+db().then(()=>{
+    // sending_data();
+    myapp.listen(port,()=>{
+        console.log(`Server Is Running On http://localhost:${port}/api/patients/`)
+    })
+}).catch((e)=>{
+    console.log(e)
+})
